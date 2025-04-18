a5e004f6e833a2c1903fd9b3cc6c1705d4dbb309
anikakash <anik15-14210@diu.eud.bd>
2025-04-18 18:36:50 +0600

    add: complete responsive menu bar

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react'
+import './Header.css'
+import { FaBars } from "react-icons/fa";
+import { FaXmark } from "react-icons/fa6";
+
+
+
+export default function Header() {
+
+    const [menuToggle, setMenuToggle] = useState(false);
+    const [currentTab, setCurrentTab] = useState("home")
+    const classNames = `nav-link ${menuToggle ? "toggle-menu" : ''}`
+
+    
+    const handleToggle = () => {
+        console.log("menus toggled.", menuToggle);
+        setMenuToggle(!menuToggle);
+    }
+
+    const handleActiveMenu = (e) =>{
+        let targetId = e.target.id || e.target.parentNode.id;
+        if(currentTab !== targetId) setCurrentTab(targetId);
+    }
+
+
+
+  return (
+    <>
+    <div className="container stickydiv">
+      <nav className="navbar flex-between">
+        <div className="logo">
+          <img src="../src/assets/logo.svg" alt="" />
+        </div>
+        <div className="menu">
+        <ul className={classNames}>
+            <li id = "home" onClick={handleActiveMenu} className={currentTab === "home" && `active`}>Home</li>
+            <li id = "doctor" onClick={handleActiveMenu} className={currentTab === "doctor" && `active`}>Find a doctor</li>
+            <li id = "app" onClick={handleActiveMenu} className={currentTab === "app" && `active`}>Apps</li>
+            <li id = "testimonial" onClick={handleActiveMenu} className={currentTab === "testimonial" && `active`}>Testimonials</li>
+            <li id = "about" onClick={handleActiveMenu} className={currentTab === "about" && `active`}>About us</li>
+          </ul>
+
+          {
+            !menuToggle ? <FaBars className='menuicon' onClick={handleToggle}/> 
+            : <FaXmark className='menuicon' onClick={handleToggle}/>
+          }
+
+        </div>
+      </nav>
+    </div>
+    </>
+  )
+}
